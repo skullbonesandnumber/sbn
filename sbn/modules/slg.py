28046c3afa760fd791a40ab062f9eddd18284b71
Bart Thate <bthate@dds.nl>
2025-02-17 20:28:21 +0100

    fix slogan

--- sbn/modules/slg.py
+++ sbn/modules/slg.py
@@ -4,7 +4,7 @@
 "slogan"
 
 
-TXT = "By law using poison, destructing in whole or in part elderly, handicapped, criminals, wicked since 4 March 2019 @KarimKhanQC OTP-CR-117/19 http://bit.ly/gcid1"
+TXT = "By law using poison, destructing in whole or in part elderly, handicapped, criminals, wicked since 4 March 2019. @KarimKhanQC OTP-CR-117/19 http://bit.ly/gcid3"
 
 
 def slg(event):
